e856ab2a3b51e7936df2b6451c349f2a1f63c2e9
mysInma <inmamayo00@gmail.com>
2022-03-15 23:24:27 +0100

    Feature 41: done

--- src/main/java/acme/entities/tools/Tool.java
+++ src/main/java/acme/entities/tools/Tool.java
@@ -1,36 +1,24 @@
-package acme.entities.components;
+package acme.entities.tools;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.tools.Tool;
 import acme.framework.entities.AbstractEntity;
-import lombok.Getter;
-import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
-
-
-public class Component extends AbstractEntity {
+public class Tool extends AbstractEntity{
 	
 	protected static final long serialVersionUID	= 1L;
 	
 	
-	//Attributes
 	@NotBlank
 	@Max(100)
 	protected String name;
+	
 	@Pattern(regexp= "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
 	@Column(unique = true)
 	protected String code;
@@ -49,19 +37,5 @@ public class Component extends AbstractEntity {
 	@URL
 	protected String link;
 	
-	
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	protected Tool tool;
-	
-	
-	/*
-	@NotNull
-    @Valid
-    @OneToOne(optional = false, mappedBy = "Patron")
 
-    protected Patron patron;
-	*/
-	
 }
