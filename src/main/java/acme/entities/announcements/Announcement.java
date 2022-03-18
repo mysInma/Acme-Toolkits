1857590e4f3b51ce97a1338a5f300f540827900c
badrijher <badrijher@alum.us.es>
2022-03-18 19:35:18 +0100

    Fix #66

--- src/main/java/acme/entities/announcements/Announcement.java
+++ src/main/java/acme/entities/announcements/Announcement.java
@@ -8,8 +8,8 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
@@ -28,8 +28,8 @@ public class Announcement extends AbstractEntity{
 
 		// Attributes -------------------------------------------------------------
 
-		@NotBlank
-		@Size(max = 101, message = "Max 101 characters")
+		@NotBlank()
+		@Length(min=1, max=100)
 		protected String				title;
 
 		@Temporal(TemporalType.TIMESTAMP)
@@ -37,15 +37,15 @@ public class Announcement extends AbstractEntity{
 		@NotNull
 		protected Date					moment;
 
+		@NotBlank()
+		@Length(min=1, max=255)
+		protected String				body;
+		
 		@NotNull
-		protected AnnouncementStatus	status;
-
-		@NotBlank
-		@Size(max = 256, message = "Max 256 characters")
-		protected String				text;
+		protected Boolean 				flag;
 
 		@URL
-		protected String				info;
+		protected String				link;
 
 
 
