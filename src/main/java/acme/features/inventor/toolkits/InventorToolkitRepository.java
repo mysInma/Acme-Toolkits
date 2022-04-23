17fd71900946ab58222b3ae54298ff2f3fc7d32b
carmen <carmengpastrana@gmail.com>
2022-04-23 19:23:06 +0200

    Task 049: Done -feature #88

---
+++ src/main/java/acme/features/inventor/toolkits/InventorToolkitRepository.java
@@ -0,0 +1,34 @@
+package acme.features.inventor.toolkits;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.toolkits.Toolkit;
+import acme.framework.datatypes.Money;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface InventorToolkitRepository extends AbstractRepository{
+		
+	@Query("SELECT t FROM Toolkit t WHERE t.inventor.id = :id")
+	Collection<Toolkit> findToolkitsByInventorId(int id);
+	
+	@Query("SELECT t FROM Toolkit t WHERE t.id = :id")
+	Toolkit findToolkitById(int id);
+	
+	/*@Query("SELECT q FROM Quantity q WHERE q.toolkit.id = :id")
+	Collection<Quantity> collectPrices(int id);*/
+	
+	@Query("SELECT q.item.price FROM Quantity q WHERE q.toolkit.id = :id")
+	Collection<Money> collectPrices(int id);
+	
+	/*@Query("SELECT q.item FROM Quantity q WHERE q.toolkit.id = :id")
+	Collection<Item> finItemsByToolkit(int id);*/
+	
+	@Query("select c.systemCurrency from SystemConfiguration c")
+	String systemCurrency();
+
+
+}
