11ea1f0466fad3e3a7c64bb45b3806c198790ba9
mysInma <inmamayo00@gmail.com>
2022-04-23 14:38:23 +0200

    Feature #152: done

--- src/test/java/acme/testing/any/chirps/AnyListChirpsTest.java
+++ src/test/java/acme/testing/any/chirps/AnyListChirpsTest.java
@@ -11,18 +11,20 @@ public class AnyListChirpsTest extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirp/chirp.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void checkAnyListChirpsTest(final int recordIndex, final String creationMoment, final String title, 
-		final String author, final String body, final String email) {
+	public void checkAnyListChirpsTest(final int recordIndex, final String title, 
+		final String author, final String description, final String email,  final String creationMoment) {
 
 		super.clickOnMenu("Anonymous", "List Chirps");
+		super.checkListingExists();
+		super.sortListing(4, "asc"); 
 		
-		//super.sortListing(0, "asc");
 		
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
-		super.checkColumnHasValue(recordIndex, 1, title);
-		super.checkColumnHasValue(recordIndex, 2, author);
-		super.checkColumnHasValue(recordIndex, 3, body);
-		super.checkColumnHasValue(recordIndex, 4, email);
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.checkColumnHasValue(recordIndex, 1, author);
+		super.checkColumnHasValue(recordIndex, 2, description);
+		super.checkColumnHasValue(recordIndex, 3, email);
+		super.checkColumnHasValue(recordIndex, 4, creationMoment);
+		
 	
 		
 	}
