895bb0d5dfb3a1857f32b2190e010a33183ed378
javpacmar <javpacmar@alum.us.es>
2022-04-24 19:20:05 +0200

    fix(): dashboards tests

--- src/test/java/acme/testing/administrator/dashboard/AdministratorDashboardShowTest.java
+++ src/test/java/acme/testing/administrator/dashboard/AdministratorDashboardShowTest.java
@@ -1,37 +1,20 @@
 package acme.testing.administrator.dashboard;
 
-import java.util.Map;
-
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.springframework.data.util.Pair;
+import org.junit.jupiter.api.Test;
 
-import acme.entities.patronages.PatronageStatus;
 import acme.testing.TestHarness;
 
 public class AdministratorDashboardShowTest extends TestHarness {
 	
-	@ParameterizedTest
-	@CsvFileSource(resources = "/administrator/dashboard/dashboard.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Test
 	@Order(10)
-	public void positive(final Integer 								totalNumberOfComponents, 
-						final Map<Pair<String, String>, Double> 	averageRetailPriceOfComponentsByTechnologyCurrency,
-						final Map<Pair<String, String>, Double> 	deviationRetailPriceOfComponentsByTechnologyCurrency,
-						final Map<Pair<String, String>, Double> 	minimumRetailPriceOfComponentsByTechnologyCurrency,
-						final Map<Pair<String, String>, Double> 	maximumRetailPriceOfComponentsByTechnologyCurrency,
-						final Integer 								totalNumberOfTools,
-						final Map<String, Double> 					averageRetailPriceOfToolsByCurrency,
-						final Map<String, Double> 					deviationRetailPriceOfToolsByCurrency,
-						final Map<String, Double> 					minimumRetailPriceOfToolsByCurrency,
-						final Map<String, Double> 					maximumRetailPriceOfToolsByCurrency,
-						final Map<PatronageStatus, Integer> 		totalNumberOfPatronagesByStatus,
-						final Map<PatronageStatus, Double> 			averagePatronagesBudgetByStatus,
-						final Map<PatronageStatus, Double>			deviationPatronagesBudgetByStatu,
-						final Map<PatronageStatus, Double> 			minimumPatronagesBudgetByStatus,
-						final Map<PatronageStatus, Double> 			maximumPatronagesBudgetByStatus) {
+	public void positive() {
 		
 		super.signIn("administrator", "administrator");
+		
+		super.clickOnMenu("Administrator", "Dashboard");
+		super.checkCurrentPath("/administrator/administrator-dashboard/show");
 
 		super.checkFormExists();
 
