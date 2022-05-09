55804db0bee5677e9a3fbfdcb85d0a206bed2e5f
javpacmar <javpacmar@alum.us.es>
2022-05-09 17:57:39 +0200

    feat(#187): publish patronages finished

--- src/main/java/acme/features/patron/patronages/PatronPatronagePublishService.java
+++ src/main/java/acme/features/patron/patronages/PatronPatronagePublishService.java
@@ -1,5 +1,6 @@
 package acme.features.patron.patronages;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.patronages.Patronage;
@@ -12,39 +13,89 @@ import acme.roles.Patron;
 @Service
 public class PatronPatronagePublishService implements AbstractUpdateService<Patron, Patronage> {
 
+	@Autowired
+	protected PatronPatronageRepository repository;
+	
 	@Override
 	public boolean authorise(final Request<Patronage> request) {
-		// TODO Auto-generated method stub
-		return false;
+		assert request != null;
+		
+		final boolean result;
+		int patronageId;
+		Patronage patronage;
+		Patron patron;
+		
+		patronageId = request.getModel().getInteger("id");
+		patronage = this.repository.findPatronageById(patronageId);
+		patron = patronage.getPatron();
+		
+		result = patronage.isDraftMode() && request.isPrincipal(patron);
+		
+		return result;
 	}
 
 	@Override
 	public void bind(final Request<Patronage> request, final Patronage entity, final Errors errors) {
-		// TODO Auto-generated method stub
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		request.bind(entity, errors, "status", "code", "legalStuff", "budget", "creationMoment", "startDate", "finishDate", "link");
 		
 	}
 
 	@Override
 	public void unbind(final Request<Patronage> request, final Patronage entity, final Model model) {
-		// TODO Auto-generated method stub
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		
+		
+		request.unbind(entity, model, "status", "draftMode", "code", "legalStuff", "budget", "creationMoment", "startDate", "finishDate", "link");
+		
+		model.setAttribute("inventorFullName", entity.getInventor().getUserAccount().getIdentity().getFullName());
+		model.setAttribute("inventorName", entity.getInventor().getUserAccount().getIdentity().getName());
+		model.setAttribute("inventorSurname", entity.getInventor().getUserAccount().getIdentity().getSurname());
+		model.setAttribute("inventorEmail", entity.getInventor().getUserAccount().getIdentity().getEmail());
 		
 	}
 
 	@Override
 	public Patronage findOne(final Request<Patronage> request) {
-		// TODO Auto-generated method stub
-		return null;
+		assert request != null;
+
+		Patronage result;
+		int id;
+		
+		id = request.getModel().getInteger("id");
+		result = this.repository.findPatronageById(id); 
+		
+		return result;
 	}
 
 	@Override
 	public void validate(final Request<Patronage> request, final Patronage entity, final Errors errors) {
-		// TODO Auto-generated method stub
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		if (!errors.hasErrors("reference")) {
+			Patronage existing;
+
+			existing = this.repository.findPatronageByCode(entity.getCode());
+			errors.state(request, existing == null || existing.getId() == entity.getId(), "code", "patron.patronage.form.error.duplicated");
+		}
 		
 	}
 
 	@Override
 	public void update(final Request<Patronage> request, final Patronage entity) {
-		// TODO Auto-generated method stub
+		assert request != null;
+		assert entity != null;
+		
+		entity.setDraftMode(false);
+		
+		this.repository.save(entity);
 		
 	}
 
