6fc620cb7fb712299826ee2339fa20a82cbc9065
mysInma <inmamayo00@gmail.com>
2022-05-11 19:27:12 +0200

    Fix inventorItemTest: done

--- src/test/java/acme/testing/inventor/item/InventorItemTest.java
+++ src/test/java/acme/testing/inventor/item/InventorItemTest.java
@@ -12,17 +12,18 @@ public class InventorItemTest extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/item/itemComponent.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveComponentTest(final int recordIndex, final String name, final String code, final String technology, final String description, final String price, final String link) {
+	public void positiveComponentTest(final int recordIndex, final String name, final String code, final String technology, final String description, final String price, final String link, final String published) {
 
 		super.signIn("inventor01", "inventor01");
 
 		super.clickOnMenu("Inventor", "Component list");
 		super.checkListingExists();
-		super.sortListing(1, "asc");
+		super.sortListing(2, "asc");
 
 		super.checkColumnHasValue(recordIndex, 0, name);
-		super.checkColumnHasValue(recordIndex, 1, code);
-		super.checkColumnHasValue(recordIndex, 2, technology);
+		super.checkColumnHasValue(recordIndex, 1, published);
+		super.checkColumnHasValue(recordIndex, 2, code);
+		super.checkColumnHasValue(recordIndex, 3, technology);
 
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
@@ -36,23 +37,22 @@ public class InventorItemTest extends TestHarness {
 		super.signOut();
 
 	}
-	
-	
+
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/item/itemTool.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveToolTest(final int recordIndex,
-		final String name, final String code, final String technology, final String description, final String price, final String link) {
-		
+	public void positiveToolTest(final int recordIndex, final String name, final String code, final String technology, final String description, final String price, final String link, final String published) {
+
 		super.signIn("inventor02", "inventor02");
-		
-		super.clickOnMenu("Inventor","Tool list");
+
+		super.clickOnMenu("Inventor", "Tool list");
 		super.checkListingExists();
-		super.sortListing(1, "asc"); 
-		
+		super.sortListing(2, "asc");
+
 		super.checkColumnHasValue(recordIndex, 0, name);
-		super.checkColumnHasValue(recordIndex, 1, code);
-		super.checkColumnHasValue(recordIndex, 2, technology);
+		super.checkColumnHasValue(recordIndex, 1, published);
+		super.checkColumnHasValue(recordIndex, 2, code);
+		super.checkColumnHasValue(recordIndex, 3, technology);
 
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
@@ -62,13 +62,9 @@ public class InventorItemTest extends TestHarness {
 		super.checkInputBoxHasValue("description", description);
 		super.checkInputBoxHasValue("price", price);
 		super.checkInputBoxHasValue("link", link);
-		
+
 		super.signOut();
 
 	}
-	
-	
-	
-	
-	
+
 }
