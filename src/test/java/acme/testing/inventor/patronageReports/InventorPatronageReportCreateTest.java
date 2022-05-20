6344cb5b9661ae322998a0886a8aaab835c95519
Carlos <carcangom@alum.us.es>
2022-05-21 00:57:27 +0200

    Task 084/06: positive test done.

--- src/test/java/acme/testing/inventor/patronageReports/InventorPatronageReportCreateTest.java
+++ src/test/java/acme/testing/inventor/patronageReports/InventorPatronageReportCreateTest.java
@@ -7,27 +7,37 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class InventorPatronageReportTest extends TestHarness {
+public class InventorPatronageReportCreateTest extends TestHarness {
+
+	// Test cases ------------------------------
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/inventor/patronageReport/patronageReport.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/inventor/patronageReport/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String creationMoment, final String memorandum, final String link, final String patronageStatus, final String patronageCode, final String patronageLegalStuff, final String patronageBudget, final String patronageCreationMoment, final String patronageStartDate, final String patronageFinishDate, final String patronageLink) {
-
+	public void positiveTest(final int recordIndex, final String memorandum, final String link, final String confirmation, final String patronageCode, final String patronageLegalStuff, final String patronageBudget, final String patronageCreationMoment,
+		final String patronageStartDate, final String patronageFinishDate, final String patronageLink) {
 		super.signIn("inventor01", "inventor01");
 
 		super.clickOnMenu("Inventor", "Patronage reports list");
 		super.checkListingExists();
-		super.sortListing(1, "asc");
 
-		super.checkColumnHasValue(recordIndex, 0, creationMoment);
-		super.checkColumnHasValue(recordIndex, 1, memorandum);
-		super.checkColumnHasValue(recordIndex, 2, link);
+		super.clickOnButton("Create patronage report");
 
-		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
 
-		super.checkInputBoxHasValue("creationMoment", creationMoment);
+		super.fillInputBoxIn("memorandum", memorandum);
+		super.fillInputBoxIn("link", link);
+		super.fillInputBoxIn("confirmation", confirmation);
+		super.clickOnSubmit("Create Patronage Report");
+
+		super.clickOnMenu("Inventor", "Patronage reports list");
+		super.checkListingExists();
+		super.sortListing(0, "desc");
+		super.checkColumnHasValue(recordIndex, 1, memorandum);
+		super.checkColumnHasValue(recordIndex, 2, link);
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkFormExists();
 		super.checkInputBoxHasValue("memorandum", memorandum);
 		super.checkInputBoxHasValue("link", link);
 		super.checkInputBoxHasValue("patronageCode", patronageCode);
@@ -37,7 +47,9 @@ public class InventorPatronageReportTest extends TestHarness {
 		super.checkInputBoxHasValue("patronageStartDate", patronageStartDate);
 		super.checkInputBoxHasValue("patronageFinishDate", patronageFinishDate);
 		super.checkInputBoxHasValue("patronageLink", patronageLink);
-
+		
 		super.signOut();
+
 	}
+
 }
