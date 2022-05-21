ca0a9fea281c1ca45b396b951ad9a7f792d5dbfe
mysInma <inmamayo00@gmail.com>
2022-05-21 20:45:25 +0200

    Feature #235: done

--- src/main/java/acme/features/inventor/items/InventorItemsRepository.java
+++ src/main/java/acme/features/inventor/items/InventorItemsRepository.java
@@ -39,6 +39,9 @@ public interface InventorItemsRepository extends AbstractRepository {
 
 	@Query("SELECT i FROM Item i WHERE i.code = :code and i.type = 1")
 	Item findOneComponentByCode(String code);
+	
+	@Query("SELECT i FROM Item i WHERE i.code = :code")
+	Item findOneByCode(String code);
 
 	@Query("SELECT acceptedCurrencies FROM SystemConfiguration")
 	String getAcceptedCurrencies();
