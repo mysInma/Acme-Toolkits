0be4ca35954a3e21621104b08c58e38df291d5a9
albtinher <albtinher@alum.us.es>
2022-05-23 10:33:24 +0200

    Task 84/03: Negative tests

--- src/test/java/acme/testing/any/chirps/AnyCreateChirpsTest.java
+++ src/test/java/acme/testing/any/chirps/AnyCreateChirpsTest.java
@@ -41,6 +41,30 @@ public class AnyCreateChirpsTest extends TestHarness {
 		super.checkColumnHasValue(recordIndex, 3, email);
 
 	}
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/chirp/chirpNoCreate.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negativeTest(final int recordIndex, final String title, final String author,
+			final String body, final String email, final String confirmation) {
+		
+		super.clickOnMenu("Anonymous", "List Chirps");
+
+		super.checkListingExists();
+
+		super.clickOnButton("Create chirp");
+		
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("body", body);
+		super.fillInputBoxIn("email", email);
+		super.fillInputBoxIn("confirmation", confirmation);
+		
+		super.clickOnSubmit("Create");
+		
+		super.checkErrorsExist();
+	}
 
 
 }
